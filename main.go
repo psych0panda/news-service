980d23a5a7f9109c8f7fc021c5cc760fe4533bc8
igor.piatigorets <igor.piatigorets@nikel.com.ua>
2020-04-21 22:41:18 +0300

    init commit

--- main.go
+++ main.go
@@ -1,1 +1,161 @@
+package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+	"github.com/joho/godotenv"
+)
+
+type Source struct {
+	ID   interface{} `json:"id"`
+	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
+
+type Results struct {
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+	SearchKey  string
+	NextPage   int
+	TotalPages int
+	Results    Results
+}
+
+type NewsAPIError struct {
+	Status string `json:"status"`
+	Code string `json:"code"`
+	Message string `json:"message"`
+}
+
+var tmpl = template.Must(template.ParseFiles("views/index.html"))
+var apiKey *string
+
+func (a *Article) FormatPublishDate() string {
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v %d, %d", month, day, year)
+}
+
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
+func errHandler(err error, w http.ResponseWriter) {
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := tmpl.Execute(w, nil)
+	errHandler(err, w)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	errHandler(err, w)
+
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+	search := &Search{}
+	search.SearchKey = searchKey
+	next, err := strconv.Atoi(page)
+	errHandler(err, w)
+
+	search.NextPage = next
+	pageSize := 20
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	resp, err := http.Get(endpoint)
+	errHandler(err, w)
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		newsError := &NewsAPIError{}
+		err := json.NewDecoder(resp.Body).Decode(newsError)
+		if err != nil {
+			http.Error(w, "Unexpected server error", http.StatusInternalServerError)
+			return
+		}
+		http.Error(w, newsError.Message, http.StatusInternalServerError)
+	}
+	err = json.NewDecoder(resp.Body).Decode(&search.Results)
+	errHandler(err, w)
+
+	search.TotalPages = int(math.Ceil(float64(search.Results.TotalResults / pageSize)))
+	if ok := !search.IsLastPage(); ok {
+		search.NextPage++
+	}
+	err = tmpl.Execute(w, search)
+	errHandler(err, w)
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	newsApiToken, _ := os.LookupEnv("NEWS_API_TOKEN")
+	apiKey = flag.String("apiKey", newsApiToken, "Newsapi.org access key")
+	flag.Parse()
+
+	if *apiKey == "" {
+		log.Fatal("apiKey must be set")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", searchHandler)
+
+	err := http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatal("Server listening error...")
+	}
+}
