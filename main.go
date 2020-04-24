6e94182d1970acb0125f35b5a7fcf331cbde904b
igor.piatigorets <igor.piatigorets@nikel.com.ua>
2020-04-24 19:48:11 +0300

    refactoring=)

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/mattn/go-colorable"
 	logger "github.com/sirupsen/logrus"
 	"html/template"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
+	"os/signal"
 	"time"
 )
 
-type NineGagEntity struct {
+type nineGagEntity struct {
 	Status string `json:"status"`
 	Items  []struct {
 		SrcAlias     string `json:"src_alias,omitempty"`
@@ -131,73 +133,110 @@ type NineGagEntity struct {
 	} `json:"stats"`
 	SpiderName string `json:"spider_name"`
 }
+type middleware func(next http.HandlerFunc) http.HandlerFunc
 
 var tmpl = template.Must(template.ParseFiles("views/index.html"))
 var startUrl = "https://agile-river-75797.herokuapp.com/crawl.json?spider_name=ninegag&start_requests=true"
-var capacityPage = 0
+var next *string
 
-func (s *NineGagEntity) FormatPublishDate() string {
+func (s *nineGagEntity) FormatPublishDate() string {
 	date := time.Unix(int64(s.Items[0].CreationTs), 0)
 	return date.Format(time.UnixDate)
 }
 
-func (s *NineGagEntity) BuildCursor() string {
-	cursor := fmt.Sprintf("%v%%2C%v%%2C%v&c=%d", s.Items[len(s.Items)-1].ID, s.Items[len(s.Items)-2].ID, s.Items[len(s.Items)-3].ID, capacityPage+40)
-	capacityPage += 40
-	return cursor
+func nextPage() string {
+	logger.Debug(*next)
+	return *next
 }
 
-func (s *NineGagEntity) NextPage() string {
-	suffix := fmt.Sprintf("&url=https://9gag.com/v1/group-posts/group/default/type/hot?after=%s", s.BuildCursor())
-	nextPage := startUrl + suffix
-	return nextPage
+func feedHandler(w http.ResponseWriter, r *http.Request) {
+	s := &nineGagEntity{}
+
+	resp, err := http.Get(nextPage())
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		logger.Error(err)
+		return
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&s)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	suffix := "&url=https://9gag.com/v1/group-posts/group/default/type/hot?after="
+	nextCursor := fmt.Sprintf("%v%%2C%v%%2C%v&c=%d", s.Items[len(s.Items)-2].ID, s.Items[len(s.Items)-3].ID, s.Items[len(s.Items)-4].ID, s.Stats.ItemScrapedCount)
+	pn := startUrl + suffix + nextCursor
+	next = &pn
+
+	err = tmpl.Execute(w, s)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	nineGagStruct := &NineGagEntity{}
-
-	u, err := url.Parse(r.URL.String())
+func (s *nineGagEntity) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	resp, err := http.Get(startUrl)
 	if err != nil {
 		logger.Error(err)
 		return
 	}
 
-	params := u.Query()
-	page := params.Get("page")
-	if page != "" {
-		startUrl = nineGagStruct.NextPage()
-	} else {
-		page = "1"
-	}
+	defer resp.Body.Close()
 
-	//resp, err := http.Get(startUrl)
-	file, err := ioutil.ReadFile("response.json")
+	err = json.NewDecoder(resp.Body).Decode(&s)
 	if err != nil {
 		logger.Error(err)
 		return
 	}
-	_ = json.Unmarshal([]byte(file), nineGagStruct)
 
-	//defer resp.Body.Close()
-	//if resp.StatusCode != 200 {
-	//	logger.Error("Exception in response: ", http.StatusInternalServerError)
-	//	return
-	//}
+	suffix := "&url=https://9gag.com/v1/group-posts/group/default/type/hot?after="
+	nextCursor := fmt.Sprintf("%v%%2C%v%%2C%v&c=%d", s.Items[len(s.Items)-2].ID, s.Items[len(s.Items)-3].ID, s.Items[len(s.Items)-4].ID, s.Stats.ItemScrapedCount)
+	pn := startUrl + suffix + nextCursor
+	next = &pn
 
-	//err = json.NewDecoder(resp.Body).Decode(&nineGagStruct.Items)
-	//if err != nil {
-	//	logger.Error("Exception in json decode: ", err)
-	//	return
-	//}
-
-	err = tmpl.Execute(w, nineGagStruct)
+	err = tmpl.Execute(w, s)
 	if err != nil {
 		logger.Error(err)
 		return
+	}
+}
+
+func withLogging(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		logger.Debug("Connection from ", request.RemoteAddr)
+		next.ServeHTTP(writer, request)
+	}
+}
+
+func withTracing(next http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		logger.Debug("Tracing request for ", request.RequestURI)
+		next.ServeHTTP(writer, request)
+	}
+}
+
+func chainMiddleware(mw ...middleware) middleware {
+	return func(final http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, request *http.Request) {
+			last := final
+			for i := len(mw) - 1; i > 0; i-- {
+				last = mw[i](last)
+			}
+			last(writer, request)
+		}
 	}
 }
 
 func init() {
+	// Init logger
 	logger.SetFormatter(&logger.TextFormatter{ForceColors: true})
 	logger.SetOutput(colorable.NewColorableStdout())
 	logger.SetReportCaller(true)
@@ -206,19 +245,57 @@ func init() {
 
 func main() {
 	logger.Println("entry point...")
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "27010"
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*60, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	// Create Router
+	router := mux.NewRouter()
+	// Init chain middleware
+	lt := chainMiddleware(withLogging, withTracing)
+	// Structure pointer for use ServeHTTP method
+	indexHandler := &nineGagEntity{}
+	logger.Debug("Path to static: ", http.Dir("assets"))
+
+	// Registered URLs
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+	router.HandleFunc("/", lt(indexHandler.ServeHTTP))
+	router.HandleFunc("/feed/", lt(feedHandler))
+
+	// Set timeout to avoid Slowloris attacks( see https://en.wikipedia.org/wiki/Slowloris_(computer_security))
+	server := &http.Server{
+		Addr:         "127.0.0.1:27010",
+		WriteTimeout: time.Second * 60,
+		ReadTimeout:  time.Second * 60,
+		IdleTimeout:  time.Second * 60,
+		Handler:      router,
 	}
-	mux := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("assets"))
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			logger.Error(err)
+		}
+	}()
+	c := make(chan os.Signal, 1)
 
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	mux.HandleFunc("/", indexHandler)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
 
-	err := http.ListenAndServe(":"+port, mux)
-	if err != nil {
-		logger.Fatal(err)
-	}
+	// Block until we receive our signal
+	<-c
+
+	// Create a deadline to wait for
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	defer cancel()
+
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	server.Shutdown(ctx)
+	logger.Warning("Shutting down...")
+	os.Exit(0)
 }
